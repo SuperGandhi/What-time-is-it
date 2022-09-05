66621253279ee4b0c78b389dd9a9ab0aa0409655
SuperGandhi <patrickborjabueno@hotmail.com>
2022-09-04 21:45:43 -0500

    Complete

--- index.js
+++ index.js
@@ -11,5 +11,20 @@ function updateClock(){
 
     if(h > 12){
         h = h - 12
+        ampm = "PM"
     }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl, (innerText = ampm);
+    setTimeout(() => {
+        updateClock();
+    }, 1000)
 }
+
+updateClock();
